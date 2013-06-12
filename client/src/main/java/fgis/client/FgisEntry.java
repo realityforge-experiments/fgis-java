44d94f98b7646e08307ecdf009ac88dda030ece1
Peter Donald <peter@realityforge.org>
2013-06-12 15:49:10 +1000

    Add some final qualifiers

--- client/src/main/java/fgis/client/FgisEntry.java
+++ client/src/main/java/fgis/client/FgisEntry.java
@@ -4,13 +4,10 @@ import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.gwtopenmaps.openlayers.client.LonLat;
 import org.gwtopenmaps.openlayers.client.Map;
@@ -35,7 +32,7 @@ public final class FgisEntry
   {
     OpenLayers.setProxyHost( GWT.getHostPageBaseURL() + "map_proxy?targetURL=" );
     //create some MapOptions
-    MapOptions defaultMapOptions = new MapOptions();
+    final MapOptions defaultMapOptions = new MapOptions();
     //causes the mouse popup to display coordinates in this format
     defaultMapOptions.setDisplayProjection( new Projection( "EPSG:4326" ) );
     defaultMapOptions.setNumZoomLevels( 16 );
@@ -43,29 +40,27 @@ public final class FgisEntry
     //Create a MapWidget
     final MapWidget mapWidget = new MapWidget( "500px", "500px", defaultMapOptions );
     //Create a WMS layer as base layer
-    WMSParams wmsParams = new WMSParams();
+    final WMSParams wmsParams = new WMSParams();
     wmsParams.setFormat( "image/png" );
     wmsParams.setLayers( "topp:tasmania_state_boundaries" );
     wmsParams.setStyles( "" );
 
     //create a WMS layer
-    WMSOptions wmsLayerParams = new WMSOptions();
+    final WMSOptions wmsLayerParams = new WMSOptions();
     wmsLayerParams.setUntiled();
     wmsLayerParams.setTransitionEffect( TransitionEffect.RESIZE );
 
-    String wmsUrl = "http://demo.opengeo.org/geoserver/wms";
-
-    final WMS wmsLayer = new WMS( "Basic WMS", wmsUrl, wmsParams,
-                                  wmsLayerParams );
+    final String wmsUrl = "http://demo.opengeo.org/geoserver/wms";
+    final WMS wmsLayer = new WMS( "Basic WMS", wmsUrl, wmsParams, wmsLayerParams );
 
     //Add the WMS to the map
-    Map map = mapWidget.getMap();
+    final Map map = mapWidget.getMap();
     map.addLayer( wmsLayer );
 
     //The actual refresh
     final Button butRefresh = new Button( "Refresh WMS", new ClickHandler()
     {
-      public void onClick( ClickEvent event )
+      public void onClick( final ClickEvent event )
       {
         final WMSParams wmsParams = new WMSParams();
         wmsParams.setParameter( ( (Double) Math.random() ).toString(), ( (Double) Math.random() ).toString() );
@@ -75,18 +70,19 @@ public final class FgisEntry
     } );
 
     //Lets add some default controls to the map
-    //+ sign in the upperright corner to display the layer switcher
+    //+ sign in the upper right corner to display the layer switcher
     map.addControl( new LayerSwitcher() );
-    //+ sign in the lowerright to display the overviewmap
+    //+ sign in the lower right to display the overview map
     map.addControl( new OverviewMap() );
-    //Display the scaleline
+    //Display the scale line
     map.addControl( new ScaleLine() );
 
     //Center and zoom to a location
-    map.setCenter( new LonLat( 146.7, -41.8 ), 6 );
+    final LonLat center = new LonLat( 146.7, -41.8 );
+    final int zoomLevel = 6;
+    map.setCenter( center, zoomLevel );
 
     final Panel panel = new VerticalPanel();
-
 
     panel.add( mapWidget );
     panel.add( butRefresh );
@@ -95,6 +91,5 @@ public final class FgisEntry
 
     //force the map to fall behind popups
     mapWidget.getElement().getFirstChildElement().getStyle().setZIndex( 0 );
-
   }
 }
