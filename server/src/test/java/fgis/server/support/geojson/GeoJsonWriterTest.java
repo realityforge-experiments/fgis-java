5f68da62ac22d0194a3ab9dd775ea49ef5492a6f
Peter Donald <peter@realityforge.org>
2013-07-05 08:39:05 +1000

    Test feature with id

--- server/src/test/java/fgis/server/support/geojson/GeoJsonWriterTest.java
+++ server/src/test/java/fgis/server/support/geojson/GeoJsonWriterTest.java
@@ -109,6 +109,21 @@ public class GeoJsonWriterTest
     assertEquals( result, expected );
   }
 
+  @Test
+  public void emitFeatureWithId()
+  {
+    final Geometry geometry = fromWkT( "POINT (1 1)" );
+    final GjGeometry g = new GjGeometry( geometry, null, null, null );
+    final GjElement e = new GjFeature( JsonValue.TRUE, g, null, null, null );
+
+    final JsonStructure result = writeAndRead( e );
+
+    final String expectedJson =
+      "{\"type\":\"Feature\",\"id\":true,\"geometry\":{\"type\":\"Point\",\"coordinates\":[1.0,1.0]},\"properties\":{}}";
+    final JsonObject expected = (JsonObject) parse( expectedJson );
+    assertEquals( result, expected );
+  }
+
   private JsonStructure writeAndRead( final GjElement e )
   {
     final ByteArrayOutputStream baos = new ByteArrayOutputStream();
