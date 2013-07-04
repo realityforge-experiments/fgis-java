6e183db3cd689a8806a9be583b841ef0888fb793
Peter Donald <peter@realityforge.org>
2013-07-05 08:26:13 +1000

    Add test for emitting bbox

--- server/src/test/java/fgis/server/support/geojson/GeoJsonWriterTest.java
+++ server/src/test/java/fgis/server/support/geojson/GeoJsonWriterTest.java
@@ -4,14 +4,19 @@ import java.io.ByteArrayOutputStream;
 import java.io.StringReader;
 import java.nio.charset.Charset;
 import java.util.Arrays;
+import java.util.HashMap;
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonReader;
 import javax.json.JsonStructure;
+import javax.json.JsonValue;
 import javax.json.stream.JsonGenerator;
+import org.geolatte.geom.Envelope;
 import org.geolatte.geom.Geometry;
 import org.geolatte.geom.codec.Wkt;
 import org.geolatte.geom.codec.Wkt.Dialect;
+import org.geolatte.geom.crs.CrsId;
+import org.geolatte.geom.crs.CrsRegistry;
 import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
@@ -26,6 +31,19 @@ public class GeoJsonWriterTest
     final JsonStructure result = writeAndRead( e );
 
     final String expectedJson = "{\"type\":\"Point\",\"coordinates\":[1.0,1.0]}";
+    final JsonObject expected = (JsonObject) parse( expectedJson );
+    assertEquals( result, expected );
+  }
+
+  @Test
+  public void emitGeometryWithBBox()
+  {
+    final Geometry geometry = fromWkT( "POINT (1 1)" );
+    final GjElement e = new GjGeometry( geometry, null, new Envelope( 0, 0, 2, 2 ), null );
+
+    final JsonStructure result = writeAndRead( e );
+
+    final String expectedJson = "{\"type\":\"Point\",\"bbox\":[0.0,0.0,2.0,2.0],\"coordinates\":[1.0,1.0]}";
     final JsonObject expected = (JsonObject) parse( expectedJson );
     assertEquals( result, expected );
   }
