a27097618c3a810ba43e379b8aa483a668db61c8
Peter Donald <peter@realityforge.org>
2013-12-07 13:46:24 +1100

    Fixup the usage of appcache

--- server/src/main/java/fgis/server/ManifestServlet.java
+++ server/src/main/java/fgis/server/ManifestServlet.java
@@ -1,8 +1,10 @@
 package fgis.server;
 
+import javax.servlet.annotation.WebServlet;
 import org.realityforge.gwt.appcache.server.AbstractManifestServlet;
 import org.realityforge.gwt.appcache.server.propertyprovider.UserAgentPropertyProvider;
 
+@WebServlet( urlPatterns = { "/fgis.appcache" } )
 public class ManifestServlet
   extends AbstractManifestServlet
 {
--- web/src/main/webapp/gwt.jsp
+++ web/src/main/webapp/gwt.jsp
@@ -1,6 +1,6 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
-<!DOCTYPE html>
-<html>
+<!doctype html>
+<html manifest="fgis.appcache">
 <head>
   <title>FGIS</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
